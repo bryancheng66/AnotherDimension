f0a76b63a380466e28672bb856dc20f17f94c64a
bryancheng66 <bryancheng66@gmail.com>
2020-10-23 12:13:39 -0400

    Added method 'largest'

--- ArrayOps.java
+++ ArrayOps.java
@@ -1,9 +1,17 @@
 public class ArrayOps {
 	public static int sum(int[] arr){
 		int result = 0;
-		for(int i = 0; i<arr.length; i++){
+		for (int i = 0; i < arr.length; i++){
 			result += arr[i];
 		}
 		return result;
+	}
+
+	public static int largest(int[] arr){
+		int max = 0;
+		for (int i = 0; i<arr.length; i++){
+			max = arr[i] > max ? arr[i] : max;
+		}
+		return max;
 	}
 }
--- Tester.java
+++ Tester.java
@@ -2,5 +2,6 @@ public class Tester {
 	public static void main(String[] Args){
 		int[] testArray = {3,5,3,2,4,1,4};
 		System.out.println(ArrayOps.sum(testArray));
+		System.out.println(ArrayOps.largest(testArray));
 	}
 }
