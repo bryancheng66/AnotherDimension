cd10d0258e64fa2a9a3eeba657747ccfdd716189
bryancheng66 <bryancheng66@gmail.com>
2020-10-24 20:14:12 -0400

    Added method sumRows().

--- ArrayOps.java
+++ ArrayOps.java
@@ -9,9 +9,17 @@ public class ArrayOps {
 
 	public static int largest(int[] arr){
 		int max = 0;
-		for (int i = 0; i<arr.length; i++){
+		for (int i = 0; i < arr.length; i++){
 			max = arr[i] > max ? arr[i] : max;
 		}
 		return max;
+	}
+	
+	public static int[] sumRows(int[][] matrix){
+		int[] sumArray = new int[matrix.length];
+		for (int i = 0; i < matrix.length; i++){
+			sumArray[i] = sum(matrix[i]);
+		}
+		return sumArray;	
 	}
 }
--- Tester.java
+++ Tester.java
@@ -1,7 +1,16 @@
+import java.util.Arrays;
+
 public class Tester {
 	public static void main(String[] Args){
-		int[] testArray = {3,5,3,2,4,1,4};
-		System.out.println(ArrayOps.sum(testArray));
-		System.out.println(ArrayOps.largest(testArray));
+		int[] Array1 = {3,5,3,2,4,1,4};
+		int[] Array2 = {3,1,5,7,3,2};
+		int[][] Matrix1 = {{2,5,7},{3,5,2},{4,7,9}};
+		System.out.println(ArrayOps.sum(Array1));
+		System.out.println(ArrayOps.largest(Array1));
+
+		System.out.println(ArrayOps.sum(Array2));
+		System.out.println(ArrayOps.largest(Array2));
+		
+		System.out.println(Arrays.toString(ArrayOps.sumRows(Matrix1)));
 	}
 }
